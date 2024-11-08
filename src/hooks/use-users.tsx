6395a951f45ce4d78e51e72ffa2b0201ea6e9388
LovelessCodes <hello@loveless.codes>
2024-11-08 08:00:47 +0100

    feat: update useUsers hook to filter out current user from sessions and add dependency on useUser for improved user session handling

--- src/hooks/use-users.tsx
+++ src/hooks/use-users.tsx
@@ -2,14 +2,20 @@
 
 import { multiSession } from "@/utils/client";
 import { useQuery } from "@tanstack/react-query";
+import { useUser } from "./use-user";
 
 export function useUsers() {
+	const { user } = useUser();
+
 	const { data: allSessions, isLoading } = useQuery({
 		queryKey: ["users"],
 		queryFn: () => multiSession.listDeviceSessions(),
+		enabled: !!user,
 	});
 
-	const users = allSessions?.data?.map((session) => session.user);
+	const users = allSessions?.data
+		?.filter((session) => session.user.id !== user?.id)
+		.map((session) => session.user);
 
 	return {
 		users,
